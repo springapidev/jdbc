e795d643216951ae8d962aac516c0f557caaeb38
Rezaul <springapidev@gmail.com>
2017-11-16 10:46:36 +0600

    view added and project is done

--- JdbcConnection/src/com/coderbd/jdbc/service/Test.java
+++ JdbcConnection/src/com/coderbd/jdbc/service/Test.java
@@ -9,8 +9,8 @@ public class Test {
 
     public static void main(String[] args) {
         studentService = new StudentService();
-       // Student std = new Student(1003, "Ruhul");
-        // studentService.update(std);
+      Student std = new Student(1007, "Parvez");
+        studentService.save(std);
 
         //check for update
         // Student std = new Student(1003, "Ruhul Amin");
@@ -18,14 +18,14 @@ public class Test {
 //Check for delete
         // studentService.delete(101);
         //get list of student
-       List<Student> list = studentService.getStudents();
-        for (Student s : list) {
-            System.out.println("ID: " + s.getId() + " Name; " + s.getStudentName());
-        }
+       //List<Student> list = studentService.getStudents();
+       // for (Student s : list) {
+           // System.out.println("ID: " + s.getId() + " Name; " + s.getStudentName());
+       // }
              
              
         // get single student according to id
-        Student student = studentService.getStudent(1003);
-        System.out.println("ID: " + student.getId() + " Name; " + student.getStudentName());
+       // Student student = studentService.getStudent(1003);
+       // System.out.println("ID: " + student.getId() + " Name; " + student.getStudentName());
     }
 }
